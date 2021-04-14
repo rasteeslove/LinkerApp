c75f86aa369b83e5855f2332f5bd02c16c84f8e3
Rastsislau Kayko <krastsislau@gmail.com>
2021-04-14 23:25:17 +0300

    project setup

---
+++ ConsoleServerPrototype/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleServerPrototype
+{
+    /// <summary>
+    /// An enum to handle belpartizan article categories.
+    /// 
+    /// NOTE: names are the same as in belpartizan rn (could be changed to more desciptive names later).
+    /// NOTE: the enum might be expanded later bc these might not be all categories.
+    /// </summary>
+    enum Category
+    {
+        LIFE,
+        ECONOMIC,
+        BLOG,
+        OPINION,
+        ENJOY,
+        POLITIC,
+    }
+}
